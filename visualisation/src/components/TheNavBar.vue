f8b5a8e2d8c4db4e3a0407164bb4079c192c5f1a
Arpo <arpo@vanishingpoint.studio>
2024-09-13 11:44:22 +1200

    Adding in colours to navBar.
    Signed-off-by: Arpo <arpo@vanishingpoint.studio>

--- visualisation/src/components/TheNavBar.vue
+++ visualisation/src/components/TheNavBar.vue
@@ -1,47 +1,46 @@
 <template>
   <!-- Navigation Bar to allow switching between different pages in the app -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" :to="{name: 'Map'}">
-        Ōtākaro Digital Twin
-      </router-link>
-      <button class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarText"
-              aria-controls="navbarText"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Map'}">
-              Map
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'About'}">
-              About
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'License'}">
-              License
-            </router-link>
-          </li>
-        </ul>
+  <div class="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" :to="{ name: 'Map' }">
+          Ōtākaro Digital Twin
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'Map' }">
+                Map
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'About' }">
+                About
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'License' }">
+                License
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
